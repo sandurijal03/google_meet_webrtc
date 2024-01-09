b892fabfe735e5bb845e36b0eb104473678437d6
Sandip Rijal <sandurijal03@hotmail.com>
2024-01-09 16:47:16 +0545

    making leave meeting button work with both leave and cancel button working

--- public/Assets/js/app.js
+++ public/Assets/js/app.js
@@ -502,6 +502,30 @@ var MyApp = (function () {
     $('.in-call-wrap-up').hide(300)
     $('.chat-show-wrap').show(300)
   })
+  $(document).on('click', '.end-call-wrap', () => {
+    console.log('clicked')
+    $('.top-box-show')
+      .css({
+        display: 'block',
+      })
+      .html(
+        '<div class="top-box align-vertical-middle profile-dialogue-show" style="font-size:14px;"><h1 class="mt-2 text-center text-light">Leave Meeting</h1><div class="call-leave-cancel-action d-flex justify-content-center align-items-center w-100"><a href="./action.html"><button class="call-leave-action btn btn-danger mr-5">Leave</button></a><button type="button" class="call-cancel-action btn btn-secondary">Cancel</button></div></div>',
+      )
+  })
+
+  $(document).mouseup(function (e) {
+    let container = new Array()
+    container.push($('.top-box-show'))
+    $.each(container, function (key, value) {
+      if (!$(value).is(e.target) && $(value).has(e.target).length === 0) {
+        $(value).empty()
+      }
+    })
+  })
+
+  $(document).on('click', '.call-cancel-action', function () {
+    $('.top-box-show').html('')
+  })
   return {
     _init: function (uid, mid) {
       init(uid, mid)
